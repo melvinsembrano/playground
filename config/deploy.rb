48f15f6fee11f21eebe60771fe5dbdf67f394c36
Melvin Sembrano <melvinsembrano@gmail.com>
2012-09-01 10:34:06 +1000

    added deployment info

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,28 @@ require 'bundler/capistrano'
 require 'fileutils'
 
 set :application, "melvins-playground"
-set :repository,  "set your repository location here"
+set :repository,  "git@github.com:melvinsembrano/playground.git"
 
 set :scm, :git
+set :default_branch, :master
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, 'ams_deploy'
+set :deploy_to, '/home/ams_deploy/melvinsplayground.com'
 
 role :web, "melvinsplayground.com"                          # Your HTTP server, Apache/etc
 role :app, "melvinsplayground.com"                          # This may be the same as your `Web` server
 role :db,  "melvinsplayground.com", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
+
+set :deploy_via, :copy
+set :copy_cache, true
+set :copy_exclude, [".git"]
+set :keep_releases, 3
+
+ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
